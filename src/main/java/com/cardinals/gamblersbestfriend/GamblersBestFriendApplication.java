729e69fb8e0ae9779619d5e997e9210f04c5503d
bogdan-todorovic <bogdantodorovic93@gmail.com>
2021-02-10 16:16:26 +0100

    initial project setup

---
+++ src/main/java/com/cardinals/gamblersbestfriend/GamblersBestFriendApplication.java
@@ -0,0 +1,13 @@
+package com.cardinals.gamblersbestfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamblersBestFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamblersBestFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardinals/gamblersbestfriend/GamblersBestFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardinals.gamblersbestfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamblersBestFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
